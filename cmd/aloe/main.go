db7617313dba73e0beac5822300039669fd65855
Calvin Figuereo-Supraner <mail@calvin.page>
2022-08-29 21:44:27 -0700

    Add UCI call

--- cmd/aloe/main.go
+++ cmd/aloe/main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"log"
+	"os"
+
+	"github.com/clfs/aloe/engine"
+	"github.com/clfs/aloe/uci"
+)
+
 func main() {
-	println("Hello, world.")
+	eng := engine.New()
+
+	adapter := uci.NewAdapter(eng, os.Stdin, os.Stdout)
+
+	if err := adapter.Run(); err != nil {
+		log.Fatal(err)
+	}
 }
