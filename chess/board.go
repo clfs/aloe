f6daaf178eb1b4023e90ae6e8c60c21d136f433b
Calvin Figuereo-Supraner <mail@calvin.page>
2022-09-02 14:56:13 -0700

    Add more chess.Board methods

--- chess/board.go
+++ chess/board.go
@@ -10,3 +10,33 @@ type Board struct {
 	Queens  Bitboard
 	Kings   Bitboard
 }
+
+func (b *Board) Pieces(p Piece) Bitboard {
+	switch p.Role() {
+	case Pawn:
+		return b.Pawns
+	case Knight:
+		return b.Knights
+	case Bishop:
+		return b.Bishops
+	case Rook:
+		return b.Rooks
+	case Queen:
+		return b.Queens
+	case King:
+		return b.Kings
+	default:
+		panic("invalid piece")
+	}
+}
+
+func (b *Board) Color(c Color) Bitboard {
+	switch c {
+	case White:
+		return b.White
+	case Black:
+		return b.Black
+	default:
+		panic("invalid color")
+	}
+}
--- chess/color.go
+++ chess/color.go
@@ -1,7 +1,10 @@
 package chess
 
+// Color is the color of a player, piece, square, or other item. A Color is
+// either White or Black.
 type Color int8
 
+// Color constants.
 const (
 	White Color = iota
 	Black
