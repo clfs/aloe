5f44c8ea2407ec2222defa2281ee9d5abcd4bc52
Calvin Figuereo-Supraner <mail@calvin.page>
2022-09-02 13:20:50 -0700

    Remove UCI logic

--- cmd/aloe/main.go
+++ cmd/aloe/main.go
@@ -1,41 +1,9 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"log"
-	"os"
-
-	"github.com/clfs/aloe/engine"
-	"github.com/clfs/aloe/uci"
 )
 
 func main() {
-	eng := engine.New()
-
-	adapter := uci.NewAdapter(eng)
-
-	scanner := bufio.NewScanner(os.Stdin)
-
-	for scanner.Scan() {
-		line := scanner.Text()
-
-		messages, err := adapter.SendLine(line)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		for _, m := range messages {
-			response, err := m.MarshalText()
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			fmt.Printf("%s\n", response)
-		}
-	}
-
-	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
-	}
+	fmt.Println("hello world")
 }
--- engine/engine.go
+++ engine/engine.go
@@ -5,23 +5,3 @@ type Engine struct{}
 func New() *Engine {
 	return &Engine{}
 }
-
-func (e *Engine) Name() string {
-	return "aloe"
-}
-
-func (e *Engine) Author() string {
-	return "Calvin Figuereo-Supraner"
-}
-
-func (e *Engine) NewGame() error {
-	return nil // TODO: implement.
-}
-
-func (e *Engine) NewGameFromFEN(fen string) error {
-	return nil // TODO: implement.
-}
-
-func (e *Engine) MoveAlgebraic(move string) error {
-	return nil // TODO: implement.
-}
