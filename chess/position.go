0ce4b6d82193713110ea974261f1e681bacc2641
Calvin Figuereo-Supraner <mail@calvin.page>
2022-09-03 17:47:23 -0700

    Add more Position methods

--- chess/position.go
+++ chess/position.go
@@ -2,20 +2,25 @@ package chess
 
 // Position is a chess position.
 type Position struct {
-	board           Board
-	active          Color
-	castlingRights  CastlingRights
-	enPassantTarget EnPassantTarget
-	plySinceStart   uint16
-	ply50MoveRule   uint8
+	board           Board           // Piece placements.
+	sideToMove      Color           // Color of the player to move.
+	castlingRights  CastlingRights  // Set of available castling rights.
+	enPassantTarget EnPassantTarget // Target square for en passant capture, if any.
+	plySinceStart   uint16          // Number of plies since the start of the game.
+	ply50MoveRule   uint8           // Number of plies since the last capture or pawn move.
 }
 
-// Active returns the color of the active player.
-func (p *Position) Active() Color {
-	return p.active
+// SideToMove returns the color of the player to move.
+func (p *Position) SideToMove() Color {
+	return p.sideToMove
 }
 
 // Board returns the board.
 func (p *Position) Board() Board {
 	return p.board
+}
+
+// LegalMoves returns a list of legal moves.
+func (p *Position) LegalMoves() []Move {
+	return nil
 }
