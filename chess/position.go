88a2042464bcc64196e0bd44f1bdab00a14bd6e2
Calvin Figuereo-Supraner <mail@calvin.page>
2022-09-03 17:32:49 -0700

    Add chess.Position methods

--- chess/position.go
+++ chess/position.go
@@ -3,9 +3,19 @@ package chess
 // Position is a chess position.
 type Position struct {
 	board           Board
-	sideToMove      Color
+	active          Color
 	castlingRights  CastlingRights
 	enPassantTarget EnPassantTarget
 	plySinceStart   uint16
 	ply50MoveRule   uint8
+}
+
+// Active returns the color of the active player.
+func (p *Position) Active() Color {
+	return p.active
+}
+
+// Board returns the board.
+func (p *Position) Board() Board {
+	return p.board
 }
