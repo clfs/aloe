4e5d582e53b6fd4706cfb18c93d87f6d1781d763
Calvin Figuereo-Supraner <mail@calvin.page>
2022-09-18 10:58:02 -0700

    Add test suites for UCI unmarshaling

--- uci/uci_test.go
+++ uci/uci_test.go
@@ -7,48 +7,91 @@ import (
 	"github.com/google/go-cmp/cmp"
 )
 
+var testsValidPositionCommands = []struct {
+	in   string
+	want Position
+}{
+	{
+		"position startpos",
+		Position{FEN: fen.StartingFEN},
+	},
+	{
+		"position startpos moves e2e4",
+		Position{fen.StartingFEN, []string{"e2e4"}},
+	},
+	{
+		"position startpos moves e2e4 e7e5",
+		Position{fen.StartingFEN, []string{"e2e4", "e7e5"}},
+	},
+	{
+		"position fen rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
+		Position{FEN: "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"},
+	},
+	{
+		"position fen rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1 moves e2e4",
+		Position{"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", []string{"e2e4"}},
+	},
+	{
+		"position fen rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1 moves e2e4 e7e5",
+		Position{"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", []string{"e2e4", "e7e5"}},
+	},
+}
+
+var testsInvalidPositionCommands = []string{
+	"",
+	"position",
+	"position fen",
+	"position startpos moves",
+}
+
 func TestPosition_UnmarshalText(t *testing.T) {
-	cases := []struct {
-		in      []byte
-		want    Position
-		wantErr bool
-	}{
-		{
-			in:   []byte("position startpos"),
-			want: Position{FEN: fen.StartingFEN},
-		},
+	for _, tc := range testsValidPositionCommands {
+		var got Position
+		if err := got.UnmarshalText([]byte(tc.in)); err != nil {
+			t.Errorf("%q: error: %v", tc.in, err)
+		}
+		if diff := cmp.Diff(tc.want, got); diff != "" {
+			t.Errorf("%q: mismatch (-want +got):\n%s", tc.in, diff)
+		}
 	}
 
-	for _, c := range cases {
+	for _, tc := range testsInvalidPositionCommands {
 		var got Position
-		if err := got.UnmarshalText(c.in); c.wantErr != (err != nil) {
-			t.Errorf("%q: wantErr = %t, but error: %v", c.in, c.wantErr, err)
-		}
-		if diff := cmp.Diff(c.want, got); diff != "" {
-			t.Errorf("%q: (-want, +got)\n%s", c.in, diff)
+		if err := got.UnmarshalText([]byte(tc)); err == nil {
+			t.Errorf("%q: expected error, got nil", tc)
 		}
 	}
 }
 
+var testsValidGoCommands = []struct {
+	in   string
+	want Go
+}{
+	{
+		"go",
+		Go{Infinite: true},
+	},
+}
+
+var testsInvalidGoCommands = []string{
+	"",
+}
+
 func TestGo_UnmarshalText(t *testing.T) {
-	cases := []struct {
-		in      []byte
-		want    Go
-		wantErr bool
-	}{
-		{
-			in:   []byte("go"),
-			want: Go{Infinite: true},
-		},
+	for _, tc := range testsValidGoCommands {
+		var got Go
+		if err := got.UnmarshalText([]byte(tc.in)); err != nil {
+			t.Errorf("%q: error: %v", tc.in, err)
+		}
+		if diff := cmp.Diff(tc.want, got); diff != "" {
+			t.Errorf("%q: mismatch (-want +got):\n%s", tc.in, diff)
+		}
 	}
 
-	for _, c := range cases {
+	for _, tc := range testsInvalidGoCommands {
 		var got Go
-		if err := got.UnmarshalText(c.in); c.wantErr != (err != nil) {
-			t.Errorf("%q: wantErr = %t, but error: %v", c.in, c.wantErr, err)
-		}
-		if diff := cmp.Diff(c.want, got); diff != "" {
-			t.Errorf("%q: (-want, +got)\n%s", c.in, diff)
+		if err := got.UnmarshalText([]byte(tc)); err == nil {
+			t.Errorf("%q: expected error, got nil", tc)
 		}
 	}
 }
