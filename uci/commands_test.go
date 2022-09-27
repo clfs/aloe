b3e132ad72aef53c279eb17b326cae669dee862e
Calvin Figuereo-Supraner <mail@calvin.page>
2022-09-26 17:14:51 -0700

    Combine UCI marshal tests

--- uci/commands_test.go
+++ uci/commands_test.go
@@ -2,67 +2,31 @@ package uci
 
 import (
 	"bytes"
+	"encoding"
 	"testing"
 )
 
-func TestBestMove_MarshalText(t *testing.T) {
-	cases := []struct {
-		in      BestMove
-		want    []byte
-		wantErr bool
-	}{
-		{
-			in:   BestMove{Move: "e2e4"},
-			want: []byte("bestmove e2e4"),
-		},
-		{
-			in:   BestMove{Move: "e2e4", Ponder: "e7e5"},
-			want: []byte("bestmove e2e4 ponder e7e5"),
-		},
-		{
-			in:      BestMove{Ponder: "e7e5"},
-			wantErr: true,
-		},
-	}
-
-	for _, c := range cases {
-		got, err := c.in.MarshalText()
-		if c.wantErr != (err != nil) {
-			t.Errorf("%+v: wantErr = %t, err = %v", c.in, c.wantErr, err)
-		}
-		if !bytes.Equal(c.want, got) {
-			t.Errorf("%+v: want = %q, got = %q", c.in, c.want, got)
-		}
-	}
+var testsMarshalText = []struct {
+	in      encoding.TextMarshaler
+	want    []byte
+	wantErr bool
+}{
+	{in: BestMove{Move: "e2e4"}, want: []byte("bestmove e2e4")},
+	{in: BestMove{Move: "e2e4", Ponder: "e7e5"}, want: []byte("bestmove e2e4 ponder e7e5")},
+	{in: BestMove{Ponder: "e7e5"}, wantErr: true},
+	{in: ID{Name: "Skynet", Author: "Cyberdyne"}, want: []byte("id name Skynet\nid author Cyberdyne")},
+	{in: ID{Name: "Skynet"}, wantErr: true},
+	{in: ID{Author: "Cyberdyne"}, wantErr: true},
 }
 
-func TestID_MarshalText(t *testing.T) {
-	cases := []struct {
-		in      ID
-		want    []byte
-		wantErr bool
-	}{
-		{
-			in:   ID{Name: "Skynet", Author: "Cyberdyne"},
-			want: []byte("id name Skynet\nid author Cyberdyne"),
-		},
-		{
-			in:      ID{Name: "Skynet"},
-			wantErr: true,
-		},
-		{
-			in:      ID{Author: "Cyberdyne"},
-			wantErr: true,
-		},
-	}
-
-	for _, c := range cases {
-		got, err := c.in.MarshalText()
-		if c.wantErr != (err != nil) {
-			t.Errorf("%+v: wantErr = %t, err = %v", c.in, c.wantErr, err)
+func TestMarshalText(t *testing.T) {
+	for _, tc := range testsMarshalText {
+		got, err := tc.in.MarshalText()
+		if tc.wantErr != (err != nil) {
+			t.Errorf("%#v: wantErr = %t, err = %v", tc.in, tc.wantErr, err)
 		}
-		if !bytes.Equal(c.want, got) {
-			t.Errorf("%+v: want = %q, got = %q", c.in, c.want, got)
+		if !bytes.Equal(tc.want, got) {
+			t.Errorf("%#v: want = %q, got = %q", tc.in, tc.want, got)
 		}
 	}
 }
