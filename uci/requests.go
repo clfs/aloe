165317b77ef46e83f7b30350f3341f9f50a528b2
Calvin Figuereo-Supraner <mail@calvin.page>
2022-09-29 10:01:13 -0700

    Split uci commands into requests and responses

--- uci/requests.go
+++ uci/requests.go
@@ -14,32 +14,6 @@ type Request interface {
 	encoding.TextUnmarshaler
 }
 
-// A Response is a command sent from the engine to the client.
-type Response interface {
-	encoding.TextMarshaler
-}
-
-// BestMove represents the "bestmove" UCI command.
-type BestMove struct {
-	Move   string
-	Ponder string
-}
-
-func (b BestMove) MarshalText() ([]byte, error) {
-	var res []byte
-
-	if b.Move == "" {
-		return nil, fmt.Errorf("invalid bestmove: move is empty")
-	}
-
-	res = fmt.Appendf(res, "bestmove %s", b.Move)
-	if b.Ponder != "" {
-		res = fmt.Appendf(res, " ponder %s", b.Ponder)
-	}
-
-	return res, nil
-}
-
 // Go represents the "go" command.
 type Go struct {
 	SearchMoves []string // Restrict search to these moves only. Ignore if empty.
@@ -61,44 +35,6 @@ type Go struct {
 
 func (g *Go) UnmarshalText(text []byte) error {
 	return nil // TODO: implement
-}
-
-// ID represents the "id" command.
-type ID struct {
-	Name   string
-	Author string
-}
-
-func (i ID) MarshalText() ([]byte, error) {
-	var res []byte
-
-	if i.Name == "" {
-		return nil, fmt.Errorf("invalid id: name is empty")
-	}
-
-	res = fmt.Appendf(res, "id name %s\n", i.Name)
-
-	if i.Author == "" {
-		return nil, fmt.Errorf("invalid id: author is empty")
-	}
-
-	res = fmt.Appendf(res, "id author %s", i.Author)
-
-	return res, nil
-}
-
-// Score types used in [Info].
-const (
-	ScoreTypeCentipawn = "cp"
-	ScoreTypeMate      = "mate"
-)
-
-// Info represents the "info" UCI command.
-type Info struct {
-	Depth     int      // Search depth in plies.
-	PV        []string // Moves in the principal variation.
-	Score     int      // Score from the engine's point of view.
-	ScoreType string   // Either ScoreTypeCentipawn or ScoreTypeMate.
 }
 
 // Position represents the "position" command.
